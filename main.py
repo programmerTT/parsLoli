f6aef84eeebb5f6e20dc56f030d861a57feec40f
Rob <robert.grn.2007@gmail.com>
2025-08-09 23:47:36 +0500

    Remove Data

--- main.py
+++ main.py
@@ -3,10 +3,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
 import pickle
 
-data = {
-                'login': f"RGB_Printer",
-                'password': f"Mlpnko12"
-            }
 options = webdriver.FirefoxOptions()
 options.set_preference('dom.webdriver.enabled',False)
 driver = webdriver.Firefox(options=options)
@@ -16,5 +12,6 @@ wait.until(ec.title_contains('Бонусы - LoliLand'))
 
 with open('SecondAkk.pkl','wb') as f:
     pickle.dump(driver.get_cookies(), f)
+
 
 
